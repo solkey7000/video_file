8df751c74af536a4227a46d90433dbb0de4cc73a
solkey7000 <46260581+solkey7000@users.noreply.github.com>
2018-12-30 14:51:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ bot.on('ready', function () {
 
 bot.on('guildMemberAdd', member => {
     member.createDM().then(channel => {
-        return channel.send('Bienvenue sur le serveur de Alexpgm' + member.displayName)
+        return channel.send('Bienvenue sur le serveur' + member.displayName)
         console.log(`${member.displayName} à rejoind le serveur.`)
     }).catch(console.error)
 });
@@ -33,8 +33,8 @@ bot.on('message', msg => {
     if (msg.content.match(/salut/i)) {
             msg.reply('Je suis d\'accord avec toi.')
     }
-    if (msg.content === prefix + "site"){
-        msg.channel.send("https://alexpgm.000webhostapp.com/")
+    if (msg.content === prefix + "youtube"){
+        msg.channel.send("https://www.youtube.com/channel/UCE3V2oUrIlVppA8FN8H7NfA?view_as=subscriber")
         console.log("Une personne a demandé pour aller sur ton site.")
     }
 
